07f2454ad2ae5b2143f7a04a502ce6079516c2a9
mm <mm@test.com>
2014-07-15 12:18:36 +0200

    add more routes to node

--- backends/node/server.js
+++ backends/node/server.js
@@ -10,7 +10,7 @@ var router = express.Router();
 
 app.use(bodyParser());
 
-var reviewId = 32;
+var reviewId = 34;
 
 function findReview(id){
   for (var i=0; i < reviews.length; i++) {
@@ -44,8 +44,51 @@ router.get('/reviews', function(request, response){
     response.json(reviews);
   });
 
+router.put('/reviews/:id', function(request, response) {
+  response.header('Access-Control-Allow-Origin', '*');
+  console.log('in PUT request');
+  
+  var review = request.body;
+  console.log('updating book ' + JSON.stringify(review));
+  
+  var currentReview = findReview(parseInt(request.params.id, 10));
+  if (currentReview === null) {
+    response.send(404);
+  } else {
+    currentReview.rating = review.rating;
+    currentReview.comments = review.comments;
+    response.send(review);
+  }
+});
+
+router.post('/reviews/', function(request, response) {
+  response.header('Access-Control-Allow-Origin', '*');
+  var review = request.body;
+  console.log('posted review ' + JSON.stringify(review));
+  review.id = reviewId++;
+  reviews.push(review);
+  response.send(review);  
+});
+
+var allowCrossDomain = function(request, response, next) {
+  console.log('in allowCrossDomain');
+  response.header('Access-Control-Allow-Origin', '*');
+  response.header('Access-Control-Allow-Methods', 'OPTIONS, GET, PUT, POST, DELETE');
+  response.header('Access-Control-Allow-Headers', 'Content-Type');
+
+  if ('OPTIONS' == request.method) {
+    response.send(200);
+  } else {
+    next();
+  }
+};
+
+app.use(allowCrossDomain);
+
 app.use('/api', router);
 //app.use('/', router);
+
+
 
 app.listen(9000);
 
